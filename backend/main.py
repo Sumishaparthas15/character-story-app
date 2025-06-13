9226abf11dd0c7de1eb503045538368bfc55239f
Sumisha PS <sumishasudha392@gmail.com>
2025-06-14 00:45:17 +0530

    created 2nd API generate story

--- backend/main.py
+++ backend/main.py
@@ -9,6 +9,7 @@ load_dotenv()
 app = FastAPI()
 supabase_url = os.getenv("SUPABASE_URL")
 supabase_key = os.getenv("SUPABASE_KEY")
+open_api_key = os.getenv("OPENAI_API_KEY")
 
 
 supabase = create_client(supabase_url, supabase_key)
@@ -42,4 +43,33 @@ def create_character(characters:Character):
           return {"message":"charcter addedd successfully","data":respose.data}
     except Exception as e:
         print("Error occurred:", e)
+        raise HTTPException(status_code=500,detail=str(e))
+    
+
+@app.post("/api/generate_story")    
+def generate_story(payload:dict):
+    print(payload,"................................")
+    try:
+        name = payload.get("charcter_name")
+        character_data = supabase.table("characters").select("*").eq("name",name).single().execute()
+
+        if not character_data:
+            raise HTTPException(status_code=404,detail="Character not found")    
+        
+        prompt = f"Write a 5-line short story about this character: {character_data.data['details']}"
+
+        from openai import OpenAI
+        import openai
+
+        openai.api = open_api_key
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages =[{"role":"user","context":prompt}]
+        )
+         
+        story = response.choices[0].messages.content
+        return {"story":story} 
+    
+    except Exception as e:
         raise HTTPException(status_code=500,detail=str(e))
